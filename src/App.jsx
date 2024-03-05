33e9251a529713e85b3ffba01d0582f71e1cb1de
adhlira <adhliramadhan68@gmail.com>
2024-03-05 21:29:23 +0700

    fix tailwind setting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-semibold italic">Tes</h1>
+      <h1 className="text-8xl font-semibold italic">Tes</h1>
     </>
   );
 }
