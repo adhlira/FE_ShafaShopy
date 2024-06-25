c6f796476ff3db44bc3bf2901d3c7cd4181b62dd
adhlira <adhliramadhan68@gmail.com>
2024-06-25 17:09:01 +0700

    fix design add customer

--- src/Pages/AddCustomer.jsx
+++ src/Pages/AddCustomer.jsx
@@ -1,12 +1,26 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const AddCustomer = () => {
-  const [data, setData] = useState({ name: "", address: "", telp: "", level: "", status: "" });
+  const [data, setData] = useState({ name: "", address: "", telp: "", level_id: "", status: "" });
+  const [level, setLevel] = useState([]);
 
   const url = "http://localhost:4000/customers";
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/levels");
+        setLevel(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -19,7 +33,7 @@ const AddCustomer = () => {
       name: data.name,
       address: data.address,
       telp: data.telp,
-      level: +data.level,
+      level_id: +data.level_id,
       status: data.status,
     };
     try {
@@ -76,18 +90,19 @@ const AddCustomer = () => {
           />
         </div>
         <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="level">
-            Level
+          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="level_id">
+            Level Customer
           </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="level"
-            type="text"
-            name="level"
-            value={data.level}
-            onChange={handleChange}
-            placeholder="Input customer level"
-          />
+          <select className="form-select px-10 py-2 border w-full rounded-lg" name="level_id" value={data.level} onChange={handleChange}>
+            <option className="text-center md:text-base text-xs" value="">
+              Choose
+            </option>
+            {level.map((item) => (
+              <option className="md:text-base text-xs" key={item.id} value={item.id}>
+                {item.level}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="mb-4 mt-5">
           <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="status">
