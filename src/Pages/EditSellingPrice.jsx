eca691e3721a4f5632160a302d1f32033bb768e6
adhlira <adhliramadhan68@gmail.com>
2024-07-02 16:49:03 +0700

    fix design selling price

--- src/Pages/EditSellingPrice.jsx
+++ src/Pages/EditSellingPrice.jsx
@@ -56,99 +56,108 @@ const AddSellingPrice = () => {
           <button className="border rounded-lg p-2 bg-green-800 hover:bg-green-700 text-white">Back</button>
         </Link>
       </div>
-      <form className="w-full max-w-lg mx-auto p-6 rounded shadow-md" onSubmit={handleSubmit}>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="name">
-            Product Name
-          </label>
-          <select className="form-select px-10 py-2 border w-full rounded-lg cursor-not-allowed" name="product_id" value={sellingPrice?.product_id} onChange={handleChange} disabled>
-            <option className="md:text-base text-xs">{sellingPrice.Product?.name} - {sellingPrice.Product?.Color?.name}</option>
-          </select>
+      <form className="w-full mx-auto p-6 rounded shadow-md" onSubmit={handleSubmit}>
+        <div className="flex justify-between">
+          <div className="flex-1 flex-col">
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="name">
+                Product Name
+              </label>
+              <select className="form-select px-10 py-2 border w-5/6 rounded-lg cursor-not-allowed" name="product_id" value={sellingPrice?.product_id} onChange={handleChange} disabled>
+                <option className="md:text-base text-xs">
+                  {sellingPrice.Product?.name} - {sellingPrice.Product?.Color?.name}
+                </option>
+              </select>
+            </div>
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price0">
+                Price Level 0
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price0"
+                type="text"
+                name="price0"
+                value={sellingPrice.price0}
+                onChange={handleChange}
+                placeholder="Input Price Level 0"
+              />
+            </div>
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price1">
+                Price Level 1
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price1"
+                type="text"
+                name="price1"
+                value={sellingPrice.price1}
+                onChange={handleChange}
+                placeholder="Input Price Level 1"
+              />
+            </div>
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price2">
+                Price Level 2
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price2"
+                type="text"
+                name="price2"
+                value={sellingPrice.price2}
+                onChange={handleChange}
+                placeholder="Input Price Level 2"
+              />
+            </div>
+          </div>
+          <div className="flex-1 flex-col">
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price3">
+                Price Level 3
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price3"
+                type="text"
+                name="price3"
+                value={sellingPrice.price3}
+                onChange={handleChange}
+                placeholder="Input Price Level 3"
+              />
+            </div>
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price4">
+                Price Level 4
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price4"
+                type="text"
+                name="price4"
+                value={sellingPrice.price4}
+                onChange={handleChange}
+                placeholder="Input Price Level 4"
+              />
+            </div>
+            <div className="mb-4 mt-5">
+              <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price5">
+                Price Level 5
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="price5"
+                type="text"
+                name="price5"
+                value={sellingPrice.price5}
+                onChange={handleChange}
+                placeholder="Input Price Level 5"
+              />
+            </div>
+          </div>
         </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price0">
-            Price Level 0
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price0"
-            type="text"
-            name="price0"
-            value={sellingPrice.price0}
-            onChange={handleChange}
-            placeholder="Input Price Level 0"
-          />
-        </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price1">
-            Price Level 1
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price1"
-            type="text"
-            name="price1"
-            value={sellingPrice.price1}
-            onChange={handleChange}
-            placeholder="Input Price Level 1"
-          />
-        </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price2">
-            Price Level 2
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price2"
-            type="text"
-            name="price2"
-            value={sellingPrice.price2}
-            onChange={handleChange}
-            placeholder="Input Price Level 2"
-          />
-        </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price3">
-            Price Level 3
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price3"
-            type="text"
-            name="price3"
-            value={sellingPrice.price3}
-            onChange={handleChange}
-            placeholder="Input Price Level 3"
-          />
-        </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price4">
-            Price Level 4
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price4"
-            type="text"
-            name="price4"
-            value={sellingPrice.price4}
-            onChange={handleChange}
-            placeholder="Input Price Level 4"
-          />
-        </div>
-        <div className="mb-4 mt-5">
-          <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="price5">
-            Price Level 5
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="price5"
-            type="text"
-            name="price5"
-            value={sellingPrice.price5}
-            onChange={handleChange}
-            placeholder="Input Price Level 5"
-          />
-        </div>
+
         <button type="submit" className="px-10 py-2 border bg-blue-600 text-white hover:bg-blue-500 rounded mt-10 focus:outline-none focus:shadow-outline">
           Save
         </button>
