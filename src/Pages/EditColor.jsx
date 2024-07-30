6ed14d58c9f70d79c6dfdecc2785ffe0a2ab2004
adhlira <adhliramadhan68@gmail.com>
2024-07-30 11:36:57 +0700

    add alert error in add color and edit color

--- src/Pages/EditColor.jsx
+++ src/Pages/EditColor.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const EditColor = () => {
   const [color, setColor] = useState();
+  const [notification, setNotification] = useState("");
   const { id } = useParams();
   const url = `http://localhost:4000/colors/${id}`;
   const navigate = useNavigate();
@@ -38,6 +39,11 @@ const EditColor = () => {
       alert("Data Berhasil diperbarui !");
       navigate("/colors");
     } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setNotification(error.response.data.message || "Color Name is Exist");
+      } else {
+        setNotification("An error occurred on the server");
+      }
       console.log(error);
     }
   };
@@ -47,7 +53,9 @@ const EditColor = () => {
       <div className="flex justify-between">
         <h1 className="text-2xl">Edit Colors</h1>
         <Link to={"/colors"}>
-          <button className="border rounded-lg p-2 bg-green-800 hover:bg-green-700 text-white"><FaArrowLeft/></button>
+          <button className="border rounded-lg p-2 bg-green-800 hover:bg-green-700 text-white">
+            <FaArrowLeft />
+          </button>
         </Link>
       </div>
       <form className="w-full max-w-lg mx-auto p-6 rounded shadow-md" onSubmit={handleSubmit} action="">
@@ -56,7 +64,7 @@ const EditColor = () => {
             Color Name
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${notification ? "border-red-500" : ""}`}
             id="name"
             type="text"
             name="name"
@@ -65,6 +73,7 @@ const EditColor = () => {
             placeholder="Input color name"
           />
         </div>
+        {notification && <div className="mt-10 text-red-500">{notification}</div>}
         <button type="submit" className="px-4 py-2 border bg-blue-600 text-white hover:bg-blue-500 rounded mt-10 focus:outline-none focus:shadow-outline">
           Save Change
         </button>
