806b1d4aa46033d775f5094872d48c61dcddd271
adhlira <adhliramadhan68@gmail.com>
2024-08-06 20:45:23 +0700

    add error validation in edit product

--- src/Pages/EditProduct.jsx
+++ src/Pages/EditProduct.jsx
@@ -4,9 +4,11 @@ import { FaArrowLeft } from "react-icons/fa6";
 import axios from "axios";
 
 const EditProduct = () => {
-  const [product, setProduct] = useState({});
+  const [product, setProduct] = useState({ category_id: "", color_id: "", name: "", description: "", purchase_price: "", stock: "" });
   const [category, setCategory] = useState([]);
   const [color, setColor] = useState([]);
+  const [error, setError] = useState({});
+  const [notification, setNotification] = useState("");
   const { id } = useParams();
   const url = `http://localhost:4000/products/${id}`;
   const navigate = useNavigate();
@@ -55,6 +57,20 @@ const EditProduct = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const newErrors = {};
+
+    Object.keys(product).forEach((key) => {
+      if (!product[key]) {
+        newErrors[key] = "This field is required";
+      }
+    });
+
+    if (Object.keys(newErrors).length > 0) {
+      setError(newErrors);
+      setNotification("Error: Data incomplete!");
+      return;
+    }
+
     const userData = {
       category_id: +product.category_id,
       color_id: +product.color_id,
@@ -73,12 +89,18 @@ const EditProduct = () => {
     }
   };
 
+  const getFieldClassName = (field) => {
+    return error[field] ? "input-error" : "";
+  };
+
   return (
     <>
       <div className="flex justify-between">
         <h1 className="text-2xl">Edit Product</h1>
         <Link to={"/products"}>
-          <button className="border rounded-lg p-2 bg-green-800 hover:bg-green-700 text-white"><FaArrowLeft/></button>
+          <button className="border rounded-lg p-2 bg-green-800 hover:bg-green-700 text-white">
+            <FaArrowLeft />
+          </button>
         </Link>
       </div>
       <form className="w-full mx-auto p-6 rounded shadow-md" onSubmit={handleSubmit} action="">
@@ -88,7 +110,7 @@ const EditProduct = () => {
               <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="name">
                 Category Name
               </label>
-              <select className="form-select px-10 py-2 border w-5/6 rounded-lg" name="category_id" value={product?.category_id} onChange={handleChange}>
+              <select className={`form-select px-10 py-2 border w-5/6 rounded-lg ${getFieldClassName("category_id") ? "border-red-500" : ""}`} name="category_id" value={product?.category_id} onChange={handleChange}>
                 <option className="text-center md:text-base text-xs" value="">
                   Choose
                 </option>
@@ -98,12 +120,13 @@ const EditProduct = () => {
                   </option>
                 ))}
               </select>
+              {error.category_id && <div className="error text-red-500 font-thin text-sm">{error.category_id}</div>}
             </div>
             <div className="mb-4 mt-5">
               <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="name">
                 Color
               </label>
-              <select className="form-select px-10 py-2 border w-5/6 rounded-lg" name="color_id" value={product?.color_id} onChange={handleChange}>
+              <select className={`form-select px-10 py-2 border w-5/6 rounded-lg ${getFieldClassName("color_id") ? "border-red-500" : ""}`} name="color_id" value={product?.color_id} onChange={handleChange}>
                 <option className="text-center md:text-base text-xs" value="">
                   Choose
                 </option>
@@ -113,13 +136,14 @@ const EditProduct = () => {
                   </option>
                 ))}
               </select>
+              {error.color_id && <div className="error text-red-500 font-thin text-sm">{error.color_id}</div>}
             </div>
             <div className="mb-4 mt-5">
               <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="name">
                 Product Name
               </label>
               <input
-                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${getFieldClassName("name") ? "border-red-500" : ""}`}
                 id="name"
                 type="text"
                 name="name"
@@ -127,6 +151,7 @@ const EditProduct = () => {
                 onChange={handleChange}
                 placeholder="Input product name"
               />
+              {error.name && <div className="error text-red-500 font-thin text-sm">{error.name}</div>}
             </div>
           </div>
           <div className="flex-1 flex-col">
@@ -135,7 +160,7 @@ const EditProduct = () => {
                 Purchase Price
               </label>
               <input
-                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${getFieldClassName("purchase_price") ? "border-red-500" : ""}`}
                 id="purchase_price"
                 type="text"
                 name="purchase_price"
@@ -143,13 +168,14 @@ const EditProduct = () => {
                 onChange={handleChange}
                 placeholder="Input purchase price"
               />
+              {error.purchase_price && <div className="error text-red-500 font-thin text-sm">{error.purchase_price}</div>}
             </div>
             <div className="mb-4 mt-5">
               <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="stock">
                 Stock
               </label>
               <input
-                className="shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-5/6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${getFieldClassName("stock") ? "border-red-500" : ""}`}
                 id="stock"
                 type="text"
                 name="stock"
@@ -157,6 +183,7 @@ const EditProduct = () => {
                 onChange={handleChange}
                 placeholder="Input Stock"
               />
+              {error.stock && <div className="error text-red-500 font-thin text-sm">{error.stock}</div>}
             </div>
             <div className="mb-4 mt-5">
               <label className="block text-gray-700 md:text-sm text-base font-bold mb-2" htmlFor="description">
@@ -167,12 +194,13 @@ const EditProduct = () => {
                 value={product?.description}
                 onChange={handleChange}
                 placeholder="Input description"
-                className="shadow border rounded w-5/6 py-10 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+                className={`shadow border rounded w-5/6 py-10 px-3 text-gray-700 focus:outline-none focus:shadow-outline ${getFieldClassName("description") ? "border-red-500" : ""}`}
               ></textarea>
+              {error.description && <div className="error text-red-500 font-thin text-sm">{error.description}</div>}
             </div>
           </div>
         </div>
-
+        {notification && <div className="mt-10 text-red-500">{notification}</div>}
         <button type="submit" className="px-4 py-2 border bg-blue-600 text-white hover:bg-blue-500 rounded mt-10 focus:outline-none focus:shadow-outline">
           Save Change
         </button>
